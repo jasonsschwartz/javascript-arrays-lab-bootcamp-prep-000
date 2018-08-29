9b9e395a647e0b2596e601e6db13ff09c267f5c9
Jason Schwartz <jasonscottschwartz@gmail.com>
2018-08-29 12:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ function destructivelyPrependKitten(name){
   kittens.unshift(name)
   return kittens
 }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function 

c426e439a67fc2bef1dcc3e0e0efc6616fb267d1
Jason Schwartz <jasonscottschwartz@gmail.com>
2018-08-29 11:57:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Add your functions and code here
-var kittens = ["Milo", "Otis", "Garfield"]
+//var kittens = ["Milo", "Otis", "Garfield"]
 
 
 function destructivelyAppendKitten(name){
-  return kittens.append(name)
+  kittens.append(name)
+  return kittens
 }
